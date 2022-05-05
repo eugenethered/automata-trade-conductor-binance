f3ebff70551c0593dca3a80c7f2d77076d1b2762
Eugene the Red <me@eugenethered.com>
2022-05-05 20:59:21 +0100

    using improved trade executor

--- binancetrade/BinanceTradeConductor.py
+++ binancetrade/BinanceTradeConductor.py
@@ -1,4 +1,5 @@
 from trade.TradeConductor import TradeConductor
+from traderepo.repository.TradeRepository import TradeRepository
 
 from binancetrade.executor.BinanceTradeExecutor import BinanceTradeExecutor
 from binancetrade.executor.transformer.BinanceTradeTransformer import BinanceTradeTransformer
@@ -9,8 +10,9 @@ class BinanceTradeConductor:
     def __init__(self, options):
         self.options = options
         trade_transformer = BinanceTradeTransformer(self.options)
+        trade_repository = TradeRepository(self.options)
         trade_executor = BinanceTradeExecutor(self.options, trade_transformer)
-        self.trade_conductor = TradeConductor(self.options, trade_executor)
+        self.trade_conductor = TradeConductor(self.options, trade_repository, trade_executor)
 
     def conduct_trading(self):
         # todo: need simple schedular
--- tests/binancetrade/executor/handler/TradeSubmissionHandler_test.py
+++ tests/binancetrade/executor/handler/TradeSubmissionHandler_test.py
@@ -16,11 +16,12 @@ class TradeSubmissionHandlerTestCase(unittest.TestCase):
         trade_submission_handler = TradeSubmissionHandler(trade)
 
         def trade_submit_function(trade):
-            pass
+            return {'orderId': '8888-8888'}
 
         trade_submission_handler.submit_trade(trade_submit_function)
         self.assertEqual(trade.status, Status.SUBMITTED)
         self.assertEqual(trade.description, None)
+        self.assertEqual(trade.order_id, '8888-8888')
 
     def test_should_raise_exception_when_account_has_insufficient_amount_to_trade(self):
         trade = InstrumentTrade('USDT', 'BTC', 100)
@@ -30,11 +31,11 @@ class TradeSubmissionHandlerTestCase(unittest.TestCase):
         trade_submission_handler = TradeSubmissionHandler(trade)
 
         def trade_submit_function(trade):
-            raise ClientError(error_code=-2010, error_message='Account has insufficient balance for requested action.', status_code=None, header=None)
+            raise ClientError(error_code=-1013, error_message='Account has insufficient balance for requested action.', status_code=None, header=None)
 
         trade_submission_handler.submit_trade(trade_submit_function)
         self.assertEqual(trade.status, Status.ERROR)
-        self.assertEqual(trade.description, 'Account has insufficient balance for requested action.')
+        self.assertEqual(trade.description, 'Not Enough Funds - Account has insufficient balance for requested action. [-1013]')
 
 
 if __name__ == '__main__':
