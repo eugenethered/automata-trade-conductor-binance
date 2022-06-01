cad6c8fd5cfc30e068978b239d50be551589118f
Eugene the Red <me@eugenethered.com>
2022-06-01 14:50:30 +0100

    update deps

--- binancetrade/__main__.py
+++ binancetrade/__main__.py
@@ -4,6 +4,7 @@ from cache.holder.RedisCacheHolder import RedisCacheHolder
 from config.report.holder.ConfigReporterHolder import ConfigReporterHolder
 from core.arguments.command_line_arguments import option_arg_parser
 from logger.ConfigureLogger import ConfigureLogger
+from metainfo.MetaInfo import MetaInfo
 
 from binancetrade.BinanceTradeConductor import BinanceTradeConductor
 
@@ -11,7 +12,9 @@ from binancetrade.BinanceTradeConductor import BinanceTradeConductor
 def start():
     ConfigureLogger()
 
-    command_line_arg_parser = option_arg_parser('persuader-technology-automata-trade-conductor-binance')
+    meta_info = MetaInfo('persuader-technology-automata-trade-conductor-binance')
+
+    command_line_arg_parser = option_arg_parser(meta_info)
     args = command_line_arg_parser.parse_args()
 
     log = logging.getLogger('Binance Position Conductor')
