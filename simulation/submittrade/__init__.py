296539681b6ed99b81059fed2e3523ecfba7a437
Eugene the Red <me@eugenethered.com>
2022-07-29 18:24:17 +0100

    bump version + deps

--- simulation/submittrade/__init__.py
+++ simulation/submittrade/__init__.py
@@ -6,14 +6,14 @@ from simulation.submittrade.TradeCreatorAndSubmitter import TradeCreatorAndSubmi
 if __name__ == '__main__':
 
     options = {
-        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_ADDRESS': '10.104.71.60',
         'REDIS_SERVER_PORT': 6379,
         'TRADE_TRANSFORMATIONS_KEY': 'binance:transformation:trade',
         'TRADE_KEY': 'test:trade',
         'MISSING_KEY': 'binance:missing',
         'AUTH_INFO_KEY': 'binance:auth:info',
-        'PROCESS_KEY': '{}:process:status:{}',
-        'PROCESS_RUN_PROFILE_KEY': '{}:process:run-profile:{}'
+        'PROCESS_KEY': 'binance:process:mv:status',
+        'PROCESS_RUN_PROFILE_KEY': 'binance:process:mv:run-profile'
     }
 
     RedisCacheHolder(options)
